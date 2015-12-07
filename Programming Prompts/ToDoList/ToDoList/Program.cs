7f72a1d33e1a4ea6d0e693b90ab07cb34e4e759f
Daniel Kloss <arsenal_fan_dan@hotmail.com>
2015-12-07 13:57:24 +0000

    Added ToDo List

---
+++ Programming Prompts/ToDoList/ToDoList/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList
+{
+    class Program
+    {
+        static ToDoList list = new ToDoList();
+
+        static void Main()
+        {
+            while (true)
+            {
+                ViewMainMenu();
+            }
+        }
+
+        private static void ViewMainMenu()
+        {
+            Console.WriteLine("1 - View List");
+            Console.WriteLine("2 - Load List");
+            Console.WriteLine("3 - Save List");
+            Console.WriteLine("4 - Add Task To List");
+            Console.WriteLine("5 - Delete Task From List");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    ViewList();
+                    break;
+                case "2":
+                    LoadList();
+                    break;
+                case "3":
+                    SaveList();
+                    break;
+                case "4":
+                    AddToList();
+                    break;
+                case "5":
+                    DeleteFromList();
+                    break;
+            }
+        }
+
+        private static void DeleteFromList()
+        {
+            Console.Clear();
+            Console.WriteLine("Name of task to delete:");
+            bool result = list.Remove(Console.ReadLine());
+
+            if (result)
+            {
+                Console.WriteLine("Task Deleted");
+            }
+            else
+            {
+                Console.WriteLine("Task Dosen't Exist");
+            }
+
+            Console.WriteLine();
+        }
+
+        private static void AddToList()
+        {
+            Console.Clear();
+            Console.WriteLine("Name of task to add:");
+            list.Add(Console.ReadLine());
+            Console.WriteLine("Task Added");
+            Console.WriteLine();
+        }
+
+        private static void SaveList()
+        {
+            Console.Clear();
+            Console.WriteLine("Specify path to save to:");
+            bool result = list.SaveList(Console.ReadLine());
+
+            if (result)
+            {
+                Console.WriteLine("List Saved");
+            }
+            else
+            {
+                Console.WriteLine("Couldn't save list, please check your path and try again.");
+            }
+
+            Console.WriteLine();
+        }
+
+        private static void LoadList()
+        {
+            Console.Clear();
+            Console.WriteLine("Specify file path to load:");
+            bool result = list.LoadList(Console.ReadLine());
+
+            if (result)
+            {
+                Console.WriteLine("List Loaded");
+            }
+            else
+            {
+                Console.WriteLine("Couldn't load list, please check your path and try again.");
+            }
+
+            Console.WriteLine();
+        }
+
+        static void ViewList()
+        {
+            Console.Clear();
+
+            foreach (string item in list.ToDos)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Programming Prompts/ToDoList/ToDoList/ToDoList.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList
+{
+    public class ToDoList
+    {
+        public List<string> ToDos = new List<string>();
+
+        public bool LoadList(string path)
+        {
+            string[] fileLines;
+
+            try
+            {
+                fileLines = File.ReadAllLines(path);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            foreach (string fileLine in fileLines)
+            {
+                ToDos.Add(fileLine.Trim());
+            }
+
+            return true;
+        }
+
+        public bool SaveList(string path)
+        {
+            try
+            {
+                File.WriteAllLines(path, ToDos);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public void Add(string todo)
+        {
+            ToDos.Add(todo);
+        }
+
+        public bool Remove(string todo)
+        {
+            if (ToDos.Contains(todo))
+            {
+                ToDos.RemoveAll(t => t == todo);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
