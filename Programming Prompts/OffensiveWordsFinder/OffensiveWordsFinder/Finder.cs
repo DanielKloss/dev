3ed60ff72f7bcc85062424cf8fcbebbbcb439065
Daniel Kloss <arsenal_fan_dan@hotmail.com>
2015-12-07 13:05:52 +0000

    Added Offensive Word Finder

---
+++ Programming Prompts/OffensiveWordsFinder/OffensiveWordsFinder/Finder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OffensiveWordsFinder
+{
+    public static class Finder
+    {
+        public static bool CheckWordForOffense(string input, string offensiveWord)
+        {
+            //Put word into an array of characters
+            char[] letters = input.ToCharArray();
+
+            //Find all characters in the word that also appear in the offensive word
+            char[] offensiveLettersFound = letters.Where(c => offensiveWord.Contains(c)).ToArray();
+
+            //Convert the charactersfound into a word
+            string offensiveWordFound = string.Concat(offensiveLettersFound);
+
+            //Check if that word is offensive
+            if (offensiveWordFound == offensiveWord)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Programming Prompts/OffensiveWordsFinder/OffensiveWordsFinder/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OffensiveWordsFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> wordsToCheck = new List<string>
+            {
+                "synchronized",
+                "misfunctioned",
+                "mispronounced",
+                "shotgunned",
+                "snond"
+            };
+
+            string offensiveWord = "snond";
+
+            foreach (string word in wordsToCheck)
+            {
+                Console.WriteLine(word + " " + Finder.CheckWordForOffense(word, offensiveWord));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
