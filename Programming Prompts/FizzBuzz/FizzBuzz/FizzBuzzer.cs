56132d0db97c8d1e9667a731e3cdff0785abe714
Daniel Kloss <arsenal_fan_dan@hotmail.com>
2015-12-07 13:10:12 +0000

    Added FizzBuzz

---
+++ Programming Prompts/FizzBuzz/FizzBuzz/FizzBuzzer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class FizzBuzzer
+    {
+        string fizzBuzz = "FizzBuzz";
+        string fizz = "Fizz";
+        string buzz = "Buzz";
+
+        public string Check(int input)
+        {
+            string output;
+
+            if (input % 15 == 0)
+            {
+                output = fizzBuzz;
+            }
+            else if (input % 3 == 0)
+            {
+                output = fizz;
+            }
+            else if (input % 5 == 0)
+            {
+                output = buzz;
+            }
+            else
+            {
+                output = input.ToString();
+            }
+
+            return output;
+        }
+    }
+}
---
+++ Programming Prompts/FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzzer fizzBuzzer = new FizzBuzzer();
+
+            for (int i = 1; i <= 100; i++)
+            {
+                string result = fizzBuzzer.Check(i);
+                Console.WriteLine(result);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
