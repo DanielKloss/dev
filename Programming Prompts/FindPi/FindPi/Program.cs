6d5c1e0da8d9e7777a481a1877e2d78f4f4f4fb2
Daniel Kloss <arsenal_fan_dan@hotmail.com>
2015-12-07 13:08:58 +0000

    Added Pi Finder

---
+++ Programming Prompts/FindPi/FindPi/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindPi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> circlesToScan = new List<string>
+            {
+                "circle1.png",
+                "circle2.png"
+            };
+
+            foreach (string circlePath in circlesToScan)
+            {
+                Console.WriteLine(CircleScanner.ScanCircle(circlePath));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
