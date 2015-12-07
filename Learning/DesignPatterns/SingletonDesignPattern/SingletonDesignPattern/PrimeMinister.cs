e2059a4bf6abab094d684724d15cdd11e028fa3f
Daniel Kloss <arsenal_fan_dan@hotmail.com>
2015-12-07 13:00:34 +0000

    Added Singleton Design Pattern

---
+++ Learning/DesignPatterns/SingletonDesignPattern/SingletonDesignPattern/PrimeMinister.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDesignPattern
+{
+    public sealed class PrimeMinister
+    {
+        private readonly static PrimeMinister _instance = new PrimeMinister();
+        public static PrimeMinister Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+        public string Name { get; set; }
+
+        PrimeMinister()
+        {
+            Name = "David Cameron";
+        }
+    }
+}
---
+++ Learning/DesignPatterns/SingletonDesignPattern/SingletonDesignPattern/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrimeMinister pm = PrimeMinister.Instance;
+
+            Console.WriteLine(pm.Name);
+            Console.ReadLine();
+
+            pm.Name = "Daniel Kloss";
+            Console.WriteLine(pm.Name);
+            Console.ReadLine();
+
+            //PrimeMinister.Instance = new PrimeMinister();
+            //pm = new PrimeMinister();
+        }
+    }
+}
