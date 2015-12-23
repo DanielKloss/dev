aaf47dca19a7cda1a6cbc5ca49aa6ca234e1e9cc
Daniel Kloss <daniel.kloss@fdmgroup.com>
2015-12-23 10:44:39 +0000

    FinishedDayOne

---
+++ AdventOfCode/AdventOfCode.Domain/DayOne.cs
@@ -0,0 +1,42 @@
+﻿
+namespace AdventOfCode.Domain
+{
+    public class DayOne
+    {
+        public int floor { get; set; }
+        public bool basementFound { get; set; }
+        public int basementInstruction { get; set; }
+
+        public DayOne()
+        {
+            floor = 0;
+            basementFound = false;
+            basementInstruction = 0;
+        }
+
+        public void FindFloor(string instructions)
+        {
+            int index = 1;
+
+            foreach (char instruction in instructions)
+            {
+                if (instruction == '(')
+                {
+                    floor++;
+                }
+                else if (instruction == ')')
+                {
+                    floor--;
+
+                    if (floor == -1 && !basementFound)
+                    {
+                        basementInstruction = index;
+                        basementFound = true;
+                    }
+                }
+
+                index++;
+            }
+        }
+    }
+}
---
+++ AdventOfCode/AdventOfCode.Test/DayOneTest.cs
@@ -0,0 +1,146 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AdventOfCode.Domain;
+
+namespace AdventOfCode.Test
+{
+    [TestClass]
+    public class DayOneTest
+    {
+        DayOne dayOne;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            dayOne = new DayOne();
+        }
+
+        [TestMethod]
+        public void Test_StartsOnFloorZero()
+        {
+            //Arrange
+            int expected = 0;
+
+            //Act
+
+            //Assert
+            Assert.AreEqual(expected, dayOne.floor);
+        }
+
+        [TestMethod]
+        public void Test_FindFloor_SantaDoesntMove_WhenGivenNoInstructions()
+        {
+            //Arrange
+            int expected = 0;
+
+            //Act
+            dayOne.FindFloor("");
+
+            //Assert
+            Assert.AreEqual(expected, dayOne.floor);
+        }
+
+        [TestMethod]
+        public void Test_FindFloor_SantaGoesUp_WhenGivenLeftBracket()
+        {
+            //Arrange
+            int expected = 1;
+
+            //Act
+            dayOne.FindFloor("(");
+
+            //Assert
+            Assert.AreEqual(expected, dayOne.floor);
+        }
+
+        [TestMethod]
+        public void Test_FindFloor_SantaGoesDown_WhenGivenRightBracket()
+        {
+            //Arrange
+            int expected = -1;
+
+            //Act
+            dayOne.FindFloor(")");
+
+            //Assert
+            Assert.AreEqual(expected, dayOne.floor);
+        }
+
+        [TestMethod]
+        public void Test_FindFloor_SantaDoesntMove_WhenGivenRightAndLeftBracket()
+        {
+            //Arrange
+            int expected = 0;
+
+            //Act
+            dayOne.FindFloor("()");
+
+            //Assert
+            Assert.AreEqual(expected, dayOne.floor);
+        }
+
+        [TestMethod]
+        public void Test_FindFloor_SantaGoesUpThree_WhenGivenThreeLeftBrackets()
+        {
+            //Arrange
+            int expected = 3;
+
+            //Act
+            dayOne.FindFloor("(((");
+
+            //Assert
+            Assert.AreEqual(expected, dayOne.floor);
+        }
+
+        [TestMethod]
+        public void Test_FindFloor_SantaGoesDownThree_WhenGivenThreeRightBrackets()
+        {
+            //Arrange
+            int expected = -3;
+
+            //Act
+            dayOne.FindFloor(")))");
+
+            //Assert
+            Assert.AreEqual(expected, dayOne.floor);
+        }
+
+        [TestMethod]
+        public void Test_FindFloor_SantaGoesDownThree_WhenGivenThreeLeftAndSixRightBrackets()
+        {
+            //Arrange
+            int expected = -3;
+
+            //Act
+            dayOne.FindFloor("()))(()))");
+
+            //Assert
+            Assert.AreEqual(expected, dayOne.floor);
+        }
+
+        [TestMethod]
+        public void Test_FindFloor_BasementFoundOnFirstInstruction_WhenGivenOneRightBracket()
+        {
+            //Arrange
+            int expected = 1;
+
+            //Act
+            dayOne.FindFloor(")");
+
+            //Assert
+            Assert.AreEqual(expected, dayOne.basementInstruction);
+        }
+
+        [TestMethod]
+        public void Test_FindBasement_BasementFoundOnFifthInstruction_WhenGivenThreeRightAndTwoLeftBrackets()
+        {
+            //Arrange
+            int expected = 5;
+
+            //Act
+            dayOne.FindFloor("()())");
+
+            //Assert
+            Assert.AreEqual(expected, dayOne.basementInstruction);
+        }
+    }
+}
