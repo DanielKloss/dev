490dcebf81640c98f96aa889a59abe723a82c559
danielkloss <arsenal_fan_dan@hotmail.com>
2016-12-01 11:55:46 +0000

    Day 1 Part 1 Solved

--- AdventOfCode/AdventOfCode16/AdventOfCode16.Tests/DayOneTests.cs
+++ AdventOfCode/AdventOfCode16/AdventOfCode16.Tests/DayOneTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AdventOfCode16.Domain;
 
 namespace AdventOfCode16.Tests
 {
@@ -7,8 +8,48 @@ namespace AdventOfCode16.Tests
     public class DayOneTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Test_Input_R2L3_Give5Blocks()
         {
+            //Arrange
+            int expected = 5;
+            string input = "R2, L3";
+            DayOne dayOne = new DayOne();
+
+            //Act
+            int actual = dayOne.FindHqDistance(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [TestMethod]
+        public void Test_Input_R2R2R2_Give2Blocks()
+        {
+            //Arrange
+            int expected = 2;
+            string input = "R2, R2, R2";
+            DayOne dayOne = new DayOne();
+
+            //Act
+            int actual = dayOne.FindHqDistance(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_Input_R5L5R5R3_Give12Blocks()
+        {
+            //Arrange
+            int expected = 12;
+            string input = "R5, L5, R5, R3";
+            DayOne dayOne = new DayOne();
+
+            //Act
+            int actual = dayOne.FindHqDistance(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
         }
     }
 }
--- AdventOfCode/AdventOfCode16/AdventOfCode16/DayOne.cs
+++ AdventOfCode/AdventOfCode16/AdventOfCode16/DayOne.cs
@@ -6,7 +6,89 @@ using System.Threading.Tasks;
 
 namespace AdventOfCode16.Domain
 {
-    class DayOne
+    public enum Direction
     {
+        North = 1,
+        East = 2,
+        South = 3,
+        West = 4
+    }
+
+    public class DayOne
+    {
+        private Direction currentDirection = Direction.North;
+
+        public int FindHqDistance(string input)
+        {
+            int totalVertical = 0;
+            int totalHorizontal = 0;
+            List<string> directions = SplitInstructions(input);
+
+            foreach (string direction in directions)
+            {
+                FindNewDirection(direction.Substring(0,1));
+                int blocks = Convert.ToInt32(direction.Substring(1));
+
+                switch (currentDirection)
+                {
+                    case Direction.North:
+                        totalVertical += blocks;
+                        break;
+                    case Direction.East:
+                        totalHorizontal += blocks;
+                        break;
+                    case Direction.South:
+                        totalVertical -= blocks;
+                        break;
+                    case Direction.West:
+                        totalHorizontal -= blocks;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return Math.Abs(totalVertical) + Math.Abs(totalHorizontal);
+        }
+
+        private List<string> SplitInstructions(string input)
+        {
+            List<string> directions = new List<string>();
+            List<string> instructions = input.Split(',').ToList();
+
+            foreach (string instruction in instructions)
+            {
+                string cleanInstruction = instruction.Trim(' ');
+                directions.Add(cleanInstruction);
+            }
+
+            return directions;
+        }
+
+        private void FindNewDirection(string instruction)
+        {
+            if (instruction == "R")
+            {
+                if (currentDirection == Direction.West)
+                {
+                    currentDirection = Direction.North;
+                }
+                else
+                {
+                    currentDirection = (Direction)currentDirection + 1;
+                }
+            }
+            else if (instruction == "L")
+            {
+                if (currentDirection == Direction.North)
+                {
+                    currentDirection = Direction.West;
+                }
+                else
+                {
+                    currentDirection = (Direction)currentDirection - 1;
+                }
+            }
+        }
     }
 }
