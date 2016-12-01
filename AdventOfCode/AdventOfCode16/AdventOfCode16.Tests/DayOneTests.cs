b7afc0d5a3f449033238e0985acaee198ee67024
danielkloss <arsenal_fan_dan@hotmail.com>
2016-12-01 10:44:17 +0000

    Added Structure

---
+++ AdventOfCode/AdventOfCode16/AdventOfCode16.Tests/DayOneTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode16.Tests
+{
+    [TestClass]
+    public class DayOneTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
