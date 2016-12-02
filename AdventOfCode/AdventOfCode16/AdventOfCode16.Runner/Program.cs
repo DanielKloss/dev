b87b84afb0186dbe5904030ed792c682d6dda111
danielkloss <arsenal_fan_dan@hotmail.com>
2016-12-02 15:22:20 +0000

    More refactoring

--- AdventOfCode/AdventOfCode16/AdventOfCode16.Runner/Program.cs
+++ AdventOfCode/AdventOfCode16/AdventOfCode16.Runner/Program.cs
@@ -10,23 +10,45 @@ namespace AdventOfCode16.Runner
         {
             DayOne();
 
+            DayTwo();
+
+            Console.ReadLine();
+        }
+
+        private static void Divider()
+        {
+            Console.WriteLine();
+            Console.WriteLine("-------------------------------------------------------------------------------");
+            Console.WriteLine();
+        }
+
+        private static void DayOne()
+        {
+            Console.WriteLine();
+            Console.WriteLine("*** DAY ONE ***");
+            Console.WriteLine();
+
+            string _dayOnePuzzleInput = "L5, R1, R3, L4, R3, R1, L3, L2, R3, L5, L1, L2, R5, L1, R5, R1, L4, R1, R3, L4, L1, R2, R5, R3, R1, R1, L1, R1, L1, L2, L1, R2, L5, L188, L4, R1, R4, L3, R47, R1, L1, R77, R5, L2, R1, L2, R4, L5, L1, R3, R187, L4, L3, L3, R2, L3, L5, L4, L4, R1, R5, L4, L3, L3, L3, L2, L5, R1, L2, R5, L3, L4, R4, L5, R3, R4, L2, L1, L4, R1, L3, R1, R3, L2, R1, R4, R5, L3, R5, R3, L3, R4, L2, L5, L1, L1, R3, R1, L4, R3, R3, L2, R5, R4, R1, R3, L4, R3, R3, L2, L4, L5, R1, L4, L5, R4, L2, L1, L3, L3, L5, R3, L4, L3, R5, R4, R2, L4, R2, R3, L3, R4, L1, L3, R2, R1, R5, L4, L5, L5, R4, L5, L2, L4, R4, R4, R1, L3, L2, L4, R3";
+            DayOne dayOne = new DayOne();
+            dayOne.FindHq(_dayOnePuzzleInput);
+            Console.WriteLine(string.Format("The Easter Bunny's HQ is {0} blocks away", dayOne.hqDistance));
+            Console.WriteLine(string.Format("The Easter Bunny's HQ is actually in reality {0} blocks away", dayOne.realHqDistance));
+            Divider();
+        }
+
+        private static void DayTwo()
+        {
+            Console.WriteLine();
+            Console.WriteLine("*** DAY TWO ***");
+            Console.WriteLine();
+
             string[] _dayTwoPuzzleInput = File.ReadAllLines(@"Inputs/DayTwoInput.txt");
             DayTwo dayTwo = new DayTwo();
             string code = dayTwo.FindToiletCode(_dayTwoPuzzleInput);
             string realCode = dayTwo.FindRealToiletCode(_dayTwoPuzzleInput);
             Console.WriteLine(string.Format("The code for the toilet is {0}", code));
-            Console.WriteLine(string.Format("The real code for the toilet is {0}", realCode));
-
-            Console.ReadLine();
-        }
-
-        private static void DayOne()
-        {
-            string _dayOnePuzzleInput = "L5, R1, R3, L4, R3, R1, L3, L2, R3, L5, L1, L2, R5, L1, R5, R1, L4, R1, R3, L4, L1, R2, R5, R3, R1, R1, L1, R1, L1, L2, L1, R2, L5, L188, L4, R1, R4, L3, R47, R1, L1, R77, R5, L2, R1, L2, R4, L5, L1, R3, R187, L4, L3, L3, R2, L3, L5, L4, L4, R1, R5, L4, L3, L3, L3, L2, L5, R1, L2, R5, L3, L4, R4, L5, R3, R4, L2, L1, L4, R1, L3, R1, R3, L2, R1, R4, R5, L3, R5, R3, L3, R4, L2, L5, L1, L1, R3, R1, L4, R3, R3, L2, R5, R4, R1, R3, L4, R3, R3, L2, L4, L5, R1, L4, L5, R4, L2, L1, L3, L3, L5, R3, L4, L3, R5, R4, R2, L4, R2, R3, L3, R4, L1, L3, R2, R1, R5, L4, L5, L5, R4, L5, L2, L4, R4, R4, R1, L3, L2, L4, R3";
-            DayOne dayOne = new DayOne();
-            dayOne.FindHq(_dayOnePuzzleInput);
-            Console.WriteLine(string.Format("The Easter Bunny's HQ is {0} blocks away", dayOne.hqDistance));
-            Console.WriteLine(string.Format("The Easter Bunny's HQ is actually in reality blocks away {0}", dayOne.realHqDistance));
+            Console.WriteLine(string.Format("The real code for the toilet is actually in reality {0}", realCode));
+            Divider();
         }
     }
 }
--- AdventOfCode/AdventOfCode16/AdventOfCode16/DayTwo.cs
+++ AdventOfCode/AdventOfCode16/AdventOfCode16/DayTwo.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace AdventOfCode16.Domain
 {
@@ -31,44 +29,18 @@ namespace AdventOfCode16.Domain
         {
             keypad = new List<KeyPadButton>
             {
-                new KeyPadButton("1", 1, 3),
-                new KeyPadButton("2", 2, 3),
-                new KeyPadButton("3", 3, 3),
-                new KeyPadButton("4", 1, 2),
-                new KeyPadButton("5", 2, 2),
-                new KeyPadButton("6", 3, 2),
-                new KeyPadButton("7", 1, 1),
-                new KeyPadButton("8", 2, 1),
-                new KeyPadButton("9", 3, 1)
+                new KeyPadButton("1", 1, 3), new KeyPadButton("2", 2, 3), new KeyPadButton("3", 3, 3),
+                new KeyPadButton("4", 1, 2), new KeyPadButton("5", 2, 2), new KeyPadButton("6", 3, 2),
+                new KeyPadButton("7", 1, 1), new KeyPadButton("8", 2, 1), new KeyPadButton("9", 3, 1)
             };
 
             realKeypad = new List<KeyPadButton>
             {
-                new KeyPadButton(".", 1, 1),
-                new KeyPadButton(".", 2, 1),
-                new KeyPadButton("D", 3, 1),
-                new KeyPadButton(".", 4, 1),
-                new KeyPadButton(".", 5, 1),
-                new KeyPadButton(".", 1, 2),
-                new KeyPadButton("A", 2, 2),
-                new KeyPadButton("B", 3, 2),
-                new KeyPadButton("C", 4, 2),
-                new KeyPadButton(".", 5, 2),
-                new KeyPadButton("5", 1, 3),
-                new KeyPadButton("6", 2, 3),
-                new KeyPadButton("7", 3, 3),
-                new KeyPadButton("8", 4, 3),
-                new KeyPadButton("9", 5, 3),
-                new KeyPadButton(".", 1, 4),
-                new KeyPadButton("2", 2, 4),
-                new KeyPadButton("3", 3, 4),
-                new KeyPadButton("4", 4, 4),
-                new KeyPadButton(".", 5, 4),
-                new KeyPadButton(".", 1, 5),
-                new KeyPadButton(".", 2, 5),
-                new KeyPadButton("1", 3, 5),
-                new KeyPadButton(".", 4, 5),
-                new KeyPadButton(".", 5, 5)
+                new KeyPadButton(".", 1, 5), new KeyPadButton(".", 2, 5), new KeyPadButton("1", 3, 5), new KeyPadButton(".", 4, 5), new KeyPadButton(".", 5, 5),
+                new KeyPadButton(".", 1, 4), new KeyPadButton("2", 2, 4), new KeyPadButton("3", 3, 4), new KeyPadButton("4", 4, 4), new KeyPadButton(".", 5, 4),
+                new KeyPadButton("5", 1, 3), new KeyPadButton("6", 2, 3), new KeyPadButton("7", 3, 3), new KeyPadButton("8", 4, 3), new KeyPadButton("9", 5, 3),
+                new KeyPadButton(".", 1, 2), new KeyPadButton("A", 2, 2), new KeyPadButton("B", 3, 2), new KeyPadButton("C", 4, 2), new KeyPadButton(".", 5, 2),
+                new KeyPadButton(".", 1, 1), new KeyPadButton(".", 2, 1), new KeyPadButton("D", 3, 1), new KeyPadButton(".", 4, 1), new KeyPadButton(".", 5, 1)
             };
         }
 
@@ -79,55 +51,9 @@ namespace AdventOfCode16.Domain
 
             StringBuilder builder = new StringBuilder();
 
-            foreach (string instruction in input)
-            {
-                foreach (char direction in instruction)
-                {
-                    switch (direction)
-                    {
-                        case 'U':
-                            currentY = increase(currentY, 3);
-                            break;
-                        case 'D':
-                            currentY = decrease(currentY, 1);
-                            break;
-                        case 'R':
-                            currentX = increase(currentX, 3);
-                            break;
-                        case 'L':
-                            currentX = decrease(currentX, 1);
-                            break;
-                    }
-                }
-
-                builder.Append(keypad.Single(k => k.x == currentX && k.y == currentY).value);
-            }
+            FindKeys(input, builder, keypad);
 
             return builder.ToString();
-        }
-
-        private int increase(int current, int upperLimit)
-        {
-            if (current != upperLimit)
-            {
-                return ++current;
-            }
-            else
-            {
-                return current;
-            }
-        }
-
-        private int decrease(int current, int lowerLimit)
-        {
-            if (current != lowerLimit)
-            {
-                return --current;
-            }
-            else
-            {
-                return current;
-            }
         }
 
         public string FindRealToiletCode(string[] input)
@@ -137,6 +63,13 @@ namespace AdventOfCode16.Domain
 
             StringBuilder builder = new StringBuilder();
 
+            FindKeys(input, builder, realKeypad);
+
+            return builder.ToString();
+        }
+
+        private void FindKeys(string[] input, StringBuilder builder, List<KeyPadButton> keypad)
+        {
             foreach (string instruction in input)
             {
                 foreach (char direction in instruction)
@@ -146,40 +79,38 @@ namespace AdventOfCode16.Domain
                     switch (direction)
                     {
                         case 'U':
-                            key = realKeypad.SingleOrDefault(k => k.x == currentX && k.y == currentY + 1);
+                            key = keypad.SingleOrDefault(k => k.x == currentX && k.y == currentY + 1);
                             if (key != null && key.value != ".")
                             {
-                                currentY = increase(currentY, 5);
+                                currentY++;
                             }
                             break;
                         case 'D':
-                            key = realKeypad.SingleOrDefault(k => k.x == currentX && k.y == currentY - 1);
+                            key = keypad.SingleOrDefault(k => k.x == currentX && k.y == currentY - 1);
                             if (key != null && key.value != ".")
                             {
-                                currentY = decrease(currentY, 1);
+                                currentY--;
                             }
                             break;
                         case 'R':
-                            key = realKeypad.SingleOrDefault(k => k.x == currentX + 1 && k.y == currentY);
+                            key = keypad.SingleOrDefault(k => k.x == currentX + 1 && k.y == currentY);
                             if (key != null && key.value != ".")
                             {
-                                currentX = increase(currentX, 5);
+                                currentX++;
                             }
                             break;
                         case 'L':
-                            key = realKeypad.SingleOrDefault(k => k.x == currentX - 1 && k.y == currentY);
+                            key = keypad.SingleOrDefault(k => k.x == currentX - 1 && k.y == currentY);
                             if (key != null && key.value != ".")
                             {
-                                currentX = decrease(currentX, 1);
+                                currentX--;
                             }
                             break;
                     }
                 }
 
-                builder.Append(realKeypad.Single(k => k.x == currentX && k.y == currentY).value);
+                builder.Append(keypad.Single(k => k.x == currentX && k.y == currentY).value);
             }
-
-            return builder.ToString();
         }
     }
 }
