f0d1b52e04a0514b5899151b1482cadc077ee3d0
danielkloss <arsenal_fan_dan@hotmail.com>
2016-12-02 09:37:29 +0000

    Refactor and structure for day two

--- AdventOfCode/AdventOfCode16/AdventOfCode16.Runner/Program.cs
+++ AdventOfCode/AdventOfCode16/AdventOfCode16.Runner/Program.cs
@@ -1,9 +1,5 @@
 ﻿using AdventOfCode16.Domain;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AdventOfCode16.Runner
 {
@@ -11,13 +7,20 @@ namespace AdventOfCode16.Runner
     {
         static void Main(string[] args)
         {
+            DayOne();
+
+
+
+            Console.ReadLine();
+        }
+
+        private static void DayOne()
+        {
             string _dayOnePuzzleInput = "L5, R1, R3, L4, R3, R1, L3, L2, R3, L5, L1, L2, R5, L1, R5, R1, L4, R1, R3, L4, L1, R2, R5, R3, R1, R1, L1, R1, L1, L2, L1, R2, L5, L188, L4, R1, R4, L3, R47, R1, L1, R77, R5, L2, R1, L2, R4, L5, L1, R3, R187, L4, L3, L3, R2, L3, L5, L4, L4, R1, R5, L4, L3, L3, L3, L2, L5, R1, L2, R5, L3, L4, R4, L5, R3, R4, L2, L1, L4, R1, L3, R1, R3, L2, R1, R4, R5, L3, R5, R3, L3, R4, L2, L5, L1, L1, R3, R1, L4, R3, R3, L2, R5, R4, R1, R3, L4, R3, R3, L2, L4, L5, R1, L4, L5, R4, L2, L1, L3, L3, L5, R3, L4, L3, R5, R4, R2, L4, R2, R3, L3, R4, L1, L3, R2, R1, R5, L4, L5, L5, R4, L5, L2, L4, R4, R4, R1, L3, L2, L4, R3";
             DayOne dayOne = new DayOne();
             dayOne.FindHq(_dayOnePuzzleInput);
-            Console.WriteLine(dayOne.hqDistance);
-            Console.WriteLine(dayOne.realHqDistance);
-
-            Console.ReadLine();
+            Console.WriteLine(string.Format("The Easter Bunny's HQ is {0} blocks away", dayOne.hqDistance));
+            Console.WriteLine(string.Format("The Easter Bunny's HQ is actually in reality blocks away {0}", dayOne.realHqDistance));
         }
     }
 }
--- AdventOfCode/AdventOfCode16/AdventOfCode16.Tests/DayOneTests.cs
+++ AdventOfCode/AdventOfCode16/AdventOfCode16.Tests/DayOneTests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using AdventOfCode16.Domain;
 
 namespace AdventOfCode16.Tests
