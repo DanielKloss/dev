69c9fcebf9b617030a3336250798db46ed6ed20f
DanielKloss <arsenal_fan_dan@hotmail.com>
2016-12-05 15:55:27 +0000

    Refactor

--- AdventOfCode/AdventOfCode16/AdventOfCode16/DayThree.cs
+++ AdventOfCode/AdventOfCode16/AdventOfCode16/DayThree.cs
@@ -8,27 +8,21 @@ namespace AdventOfCode16.Domain
 {
     public class DayThree
     {
+        List<int> sides;
+
         public int CheckTriangles(string[] input)
         {
             int triangleCount = 0;
 
             foreach (string triange in input)
             {
-                List<int> sides = new List<int>();
+                sides = new List<int>();
 
                 sides.Add(Convert.ToInt32(triange.Substring(0, 5).Trim(' ')));
                 sides.Add(Convert.ToInt32(triange.Substring(5, 5).Trim(' ')));
                 sides.Add(Convert.ToInt32(triange.Substring(10, 5).Trim(' ')));
 
-                if (sides[0] + sides[1] <= sides[2])
-                {
-                    continue;
-                }
-                else if (sides[1] + sides[2] <= sides[0])
-                {
-                    continue;
-                }
-                else if (sides[2] + sides[0] <= sides[1])
+                if (!IsValidTriangle(sides))
                 {
                     continue;
                 }
@@ -47,22 +41,14 @@ namespace AdventOfCode16.Domain
             {
                 for (int j = 0; j < input.Length; j += 3)
                 {
-                    List<int> sides = new List<int>();
+                    sides = new List<int>();
 
                     for (int k = 0; k < 3; k++)
                     {
                         sides.Add(Convert.ToInt32(input[j + k].Substring(5 * i, 5)));
                     }
 
-                    if (sides[0] + sides[1] <= sides[2])
-                    {
-                        continue;
-                    }
-                    else if (sides[1] + sides[2] <= sides[0])
-                    {
-                        continue;
-                    }
-                    else if (sides[2] + sides[0] <= sides[1])
+                    if (!IsValidTriangle(sides))
                     {
                         continue;
                     }
@@ -72,6 +58,26 @@ namespace AdventOfCode16.Domain
             }
 
             return triangleCount;
+        }
+
+        public bool IsValidTriangle(List<int> sides)
+        {
+            if (sides[0] + sides[1] <= sides[2])
+            {
+                return false;
+            }
+            else if (sides[1] + sides[2] <= sides[0])
+            {
+                return false;
+            }
+            else if (sides[2] + sides[0] <= sides[1])
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
         }
     }
 }
